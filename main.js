08cde73afd086a143aaa7d20de1bb397a8d31c4b
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-01-29 23:06:29 -0800

    Creating an Instance

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var web3 = new Web3(Web3.givenProvider);
+var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable();
+    window.ethereum.enable().then(function(accounts){
+        contractInstance = new web3.eth.Contract(abi, 
+            "0x2349468BD5B691800aA8A4677518E0f335CC836b",
+            {from: accounts[0]});
+        console.log(contractInstance);
+    });
 });
